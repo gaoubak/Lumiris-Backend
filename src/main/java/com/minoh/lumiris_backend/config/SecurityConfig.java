5b7d0a4511c7a9e952a0d5c594495e060eef5738
JinSu77 <adrien2098@hotmail.fr>
2026-05-20 15:34:35 +0200

    separate the app from the container + add actuactors

--- src/main/java/com/minoh/lumiris_backend/config/SecurityConfig.java
+++ src/main/java/com/minoh/lumiris_backend/config/SecurityConfig.java
@@ -31,7 +31,7 @@ public class SecurityConfig {
         "/swagger-ui/**",
         "/swagger-ui.html",
         "/v3/api-docs/**",
-        "/actuator/health",
+        "/actuator/**",
     };
 
     @Bean
