85d96f4c511bcc136304874a1eaf696e176d1be7
JinSu77 <adrien2098@hotmail.fr>
2026-05-20 14:47:23 +0200

    remove useless dependency in the pom.xml

--- src/main/java/com/minoh/lumiris_backend/LumirisBackendApplication.java
+++ src/main/java/com/minoh/lumiris_backend/LumirisBackendApplication.java
@@ -1,17 +1,10 @@
 package com.minoh.lumiris_backend;
 
-import org.springframework.ai.model.openai.autoconfigure.OpenAiAudioSpeechAutoConfiguration;
-import org.springframework.ai.model.openai.autoconfigure.OpenAiAudioTranscriptionAutoConfiguration;
-import org.springframework.ai.model.openai.autoconfigure.OpenAiImageAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@SpringBootApplication(exclude = {
-    OpenAiAudioSpeechAutoConfiguration.class,
-    OpenAiAudioTranscriptionAutoConfiguration.class,
-    OpenAiImageAutoConfiguration.class
-})
+@SpringBootApplication
 @EnableJpaAuditing
 public class LumirisBackendApplication {
 
--- src/test/java/com/minoh/lumiris_backend/TestLumirisBackendApplication.java
+++ src/test/java/com/minoh/lumiris_backend/TestLumirisBackendApplication.java
@@ -1,6 +1,7 @@
 package com.minoh.lumiris_backend;
 
 import org.springframework.boot.SpringApplication;
+import org.testcontainers.utility.TestcontainersConfiguration;
 
 public class TestLumirisBackendApplication {
 
